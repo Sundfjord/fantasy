58b0d1ad0f083d245891d536dc29ef812f88e74f
Yngve Sundfjord <yngve.sundfjord@jmtnordic.no>
2019-01-09 01:10:38 +0000

    Increase live league request timeout to 20 sec.

--- js/live-league.js
+++ js/live-league.js
@@ -176,7 +176,7 @@ export default {
             $.ajax({
                 url: '/fantasy/get-data.php',
                 data: payload,
-                timeout: 10000
+                timeout: 20000
             })
             .done(function(data) {
                 let result = JSON.parse(data);
